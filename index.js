438576fe5862b1323dfe40b0b6a598b9ac419146
karzan <karzanluqman99@gmail.com>
2022-08-02 16:23:43 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,33 @@
+function shout(string) {
+
+    return (string.toUpperCase());
+
+}
+function logShout(string) {
+
+    console.log(string.toUpperCase());
+
+}
+
+function whisper(string) {
+
+    return (string.toLowerCase());
+
+}
+function logWhisper(string) {
+
+    console.log(string.toLowerCase());
+
+}
+function sayHiToHeadphonedRoommate(string) {
+    if (string == string.toLowerCase()) { return ('I can\'t hear you!') }
+    if (string.toUpperCase() == string) { return ('YES INDEED!') }
+    if ("Let's have dinner together!" == string) { return ('I would love to!') }
+}
+// function sayHiToGrandma(string) {
+//     if (string.toUpperCase() == string) { console.log('YES INDEED!') }
+// }
+//function sayHiToHeadphonedRoommate(string) {
+
+
+
